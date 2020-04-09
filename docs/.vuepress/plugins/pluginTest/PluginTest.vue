c09dd6a8e60039ee4332d0efb23b602ec35c9373
Creator <382988057@qq.com>
2020-04-10 01:20:40 +0800

    update docs

--- docs/.vuepress/plugins/pluginTest/PluginTest.vue
+++ docs/.vuepress/plugins/pluginTest/PluginTest.vue
@@ -1,11 +1,12 @@
 <template>
-<div>
-
-  <div class="test">
-    {{text}}
+  
+  <div class="circle1" @click="play">
+      <div class="circle2"></div>
+       <img class="turn" src="/music.jpg" alt="">
+        <audio autoplay id="audio" ref="audio" src="https://sharefs.yun.kugou.com/202004100004/8d063674916cff635472134f597577de/G170/M07/16/11/SocBAF3H3aqAUYOEADmpdloW3bU827.mp3">
+        您的浏览器不支持 audio 标签。
+        </audio>
   </div>
-
-</div>
 </template>
 
 <script>
@@ -14,32 +15,93 @@ export default {
   data() {
     return {
       text: "这个是自定义插件-test",
+      musicUrl: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3'
     }
   },
   methods: {
-   
+    play() {
+      if(this.$refs.audio.paused) {
+         this.$refs.audio.play()
+      } else {
+         this.$refs.audio.pause()
+      }
+    }
   },
   watch: {
     $route(to, from) {
       console.log('000000')
-      getGitalk.call(this, {pages: {}})
+      getGitalk.call(this, {
+        pages: {}
+      })
     }
   },
   mounted() {
-    
+
   },
   beforeDestroy() {
-  
+
   },
 }
 </script>
 
 <style>
-.test{
-  position: absolute;
-  bottom: 100px;
-  right: 100px;
-  font-size: 40px;
-  color: red;
+.turn{
+  border-radius: 100%;
+  width: 100px;
+  height: 100px;
+  animation: pan-data-v-35d0876e 10s linear infinite;
+  z-index: 103;
+  transition: all .5s;
+  cursor: pointer;
+  transform: rotate(57deg);
+}
+ @keyframes turn{
+    0%{-webkit-transform:rotate(0deg);}
+    25%{-webkit-transform:rotate(90deg);}
+    50%{-webkit-transform:rotate(180deg);}
+    75%{-webkit-transform:rotate(270deg);}
+    100%{-webkit-transform:rotate(360deg);}
+  }
+
+.circle1 {
+  width: 100px;
+  height: 100px;
+  border-radius: 5px;
+  position: fixed;
+  z-index: 500;
+  bottom: 50px;
+  right: -50px;
+  transition: all .5s;
+	z-index:2;
+	-webkit-animation:myfirst1 5s linear infinite;
+}
+.circle1:hover{
+  right: 30px;
+}
+@-webkit-keyframes myfirst1 {
+	0% {
+	transform:rotate(0deg);
+	box-shadow:0px 0px 10px #fff;
+  }
+  50% {
+    transform:rotate(180deg);
+    box-shadow:0px 0px 45px #fff;
+  }
+  100% {
+    transform:rotate(360deg);
+    box-shadow:0px 0px 10px #fff;
+  }
+}.circle2 {
+	width:10px;
+	height:10px;
+	border:4px solid #ddd;
+	border-radius:20px;
+	left:45px;
+	top:5px;
+	position:absolute;
+	z-index:2;
+	overflow:hidden;
+	background-color:#ddd;
+	box-shadow:0px 0px 5px #fff;
 }
 </style>
